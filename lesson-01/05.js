2efc9d8ef35e41fbffa8500880341fae77ca4615
Sanych89 <sanarr89@protonmail.com>
2024-01-01 20:10:38 +0200

    -

--- lesson-01/05.js
+++ lesson-01/05.js
@@ -17,13 +17,13 @@ default:...
 
 let rule = "Еще не родился тот человек, который поставил бы цель и не смог бы стать программистом.";
 console.log(rule.length)
-switch (true) {
-    case rule.length < 25: 
-        console.log("Все таки нет правил без исключения");
-        break;         
+switch (true) {    
     case rule.length === 25:
         console.log('50 на 50');
         break; 
+    case rule.length < 25: 
+        console.log("Все таки нет правил без исключения");
+        break;         
     case rule.length > 25:
         console.log("У меня 100 пудов все получится");
         break; 
